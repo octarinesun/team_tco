b32a4985f5a6e7b10ef94badae76df29c9a10b07
alexuadler <alexuadler@users.noreply.github.com>
2018-06-14 21:14:13 -0400

    initial commit
    auto-generated feature visualization and read/clean script

---
+++ read_clean.R
@@ -0,0 +1,74 @@
+library(dplyr)
+library(lazyeval)
+library(ggplot2)
+library(magrittr)
+library(gridExtra)
+
+
+### Read Data
+load_kaggle_data = function(){
+application_test <-  read.table(file="dat/application_test.csv", sep = ",", header=T)
+saveRDS(application_test, file = "dat/application_test.RDS")
+application_train <- read.table(file="dat/application_train.csv", sep = ",", header=T)
+saveRDS(application_train, file = "dat/application_train.RDS")
+bureau <- read.table(file="dat/bureau.csv", sep = ",", header=T)
+saveRDS(bureau, file = "dat/bureau.RDS")
+bureau_balance <- read.table(file="dat/bureau_balance.csv", sep=",", header=T)
+saveRDS(bureau_balance, file = "dat/bureau_balance.RDS")
+cc_balance <- read.table(file="dat/credit_card_balance.csv", sep=",", header=T)
+saveRDS(cc_balance, file = "dat/cc_balance.RDS")
+prev_app <- read.table(file="dat/previous_application.csv", sep=",", header=T)
+saveRDS(prev_app, file = "dat/prev_app.RDS")
+}
+
+
+# Functions
+target = "TARGET"
+df = application_train
+auto_explorer = function(df,target){
+  explore_df <-  df
+  targetCol <-  which(names(df) == target)
+  
+  plot_df = NULL
+  for(i in c(1:length(explore_df))){
+    thisColNm = names(explore_df)[i]
+    if(class(df[,i]) == "factor" | n_distinct(explore_df[,i])<10){
+      p <- explore_df %>% group_by_(thisColNm) %>%
+        summarise_(sum = interp(~ sum(x), x = as.name(target)),
+                   n_entries =~n()) %>%
+        mutate(frac = sum/n_entries) %>%
+        ggplot(aes_string(x=thisColNm,y="frac")) + geom_bar(stat = "identity")
+      
+      plot_df[[i]] <- p
+      } else{next}
+  }
+  return(plot_df)
+}
+
+plots <- auto_explorer(application_train, target = "TARGET")
+
+pdf("fig/plots.pdf")
+for(p in plots){
+  print(p)
+}
+dev.off()
+
+
+p <- df %>% group_by_(thisColNm) %>%
+  summarise_(sum = interp(~ sum(x), x = as.name(target)),
+             n_entries =~n()) %>%
+  mutate(frac = sum/n_entries) %>%
+  ggplot(aes_string(x=thisColNm,y="frac")) + geom_bar(stat = "identity")
+
+# load_kagge_data()
+
+head(application_train)
+df = application_train
+
+make_df = function(df){
+  df %>% head() %>%
+    left_join(bureau, by = "SK_ID_CURR") %>% View()
+    left_join(bureau_balance, by = "SK_ID_BUREAU") %>%
+    left_join(cc_balance, by = "SK_ID_CURR")
+    
+}
