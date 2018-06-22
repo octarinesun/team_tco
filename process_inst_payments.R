56a8d9a65afd6555440d52da2d1d957f0a260e16
arnagaraja <arnagaraja@gmail.com>
2018-06-22 11:32:19 -0500

    Adding file to process installments_payments

---
+++ process_inst_payments.R
@@ -0,0 +1,57 @@
+library(dplyr)
+
+## Read data
+application_train <- readRDS("dat/application_train.RDS")
+inst_pay <- readRDS("dat/inst_pay.RDS")
+
+# FEATURE ENGINEERING
+# Somehow figure out how well someone is paying off their previous loan
+# Key:
+# Single Hash: original features
+## Double Hash: engineered feature, summary entry
+### Triple Hash: engineered feature for each row
+# SK_ID_PREV 
+# SK_ID_CURR
+# NUM_INSTALMENT_VERSION
+# NUM_INSTALMENT_NUMBER
+# DAYS_INSTALMENT
+# DAYS_ENTRY_PAYMENT
+# days_behind_schedule = DAYS_ENTRY_PAYMENT - DAYS_INSTALMENT
+# AMT_INSTALMENT
+# AMT_PAYMENT
+# amt_overpaid = AMT_PAYMENT - AMT_INSTALMENT
+# frac_pay_avg = AMT_PAYMENT / AMT_INSTALMENT
+
+# There are some entries where people have paid multiple times on a particular installment; it should be represented by a single payment for our purposes
+
+inst_pay <- inst_pay %>%
+      group_by(SK_ID_PREV, SK_ID_CURR, NUM_INSTALMENT_NUMBER,
+               NUM_INSTALMENT_VERSION, DAYS_INSTALMENT) %>%
+      summarize(DAYS_ENTRY_PAYMENT = mean(DAYS_ENTRY_PAYMENT),
+                AMT_INSTALMENT = max(AMT_INSTALMENT),
+                AMT_PAYMENT = sum(AMT_PAYMENT))
+
+dbs <- inst_pay$DAYS_ENTRY_PAYMENT - inst_pay$DAYS_INSTALMENT
+ao <- round(inst_pay$AMT_PAYMENT - inst_pay$AMT_INSTALMENT, digits = 3)
+fpa <- inst_pay$AMT_PAYMENT/inst_pay$AMT_INSTALMENT
+inst_pay <- ungroup(inst_pay) %>%
+      mutate(days_behind_schedule = dbs, amt_overpaid = ao, frac_pay_avg = fpa)
+rm(ao, dbs, fpa)
+
+# Now collapse to 1 entry per SK_ID_CURR
+
+inst_pay_summary <- inst_pay %>% 
+      group_by(SK_ID_CURR) %>% 
+      summarize(days_behind_schedule_mean = mean(days_behind_schedule),
+                days_behind_schedule_max = max(days_behind_schedule),
+                days_behind_schedule_min = min(days_behind_schedule),
+                amt_overpaid_mean = mean(amt_overpaid),
+                amt_overpaid_max = max(amt_overpaid),
+                amt_overpaid_min = min(amt_overpaid),
+                frac_pay_avg_mean = mean(frac_pay_avg),
+                frac_pay_avg_max = max(frac_pay_avg),
+                frac_pay_avg_min = min(frac_pay_avg))
+
+# Join to training set
+application_train <- left_join(application_train, inst_pay_summary, by = "SK_ID_CURR")
+rm(inst_pay, inst_pay_summary)
