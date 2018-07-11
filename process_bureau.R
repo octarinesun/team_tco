ba30e1943fefee912e82eda157bf8b6d39137979
arnagaraja <arnagaraja@gmail.com>
2018-07-10 20:58:22 -0500

    Removed valid_col function

--- process_bureau.R
+++ process_bureau.R
@@ -27,9 +27,6 @@ newFeatures <- bureau %>%
   distinct(SK_ID_CURR) %>%
   left_join(mm, by="SK_ID_CURR")
 
-valid_col = function(x, y){
-  return(!is.factor(x) & names(x[,1]) != y)
-}
 
 bureau$SK_ID_BUREAU <- as.factor(bureau$SK_ID_BUREAU)
 bureau <- bureau %>% summarize_if(is.numeric, funs(max, min, mean), na.rm=T)
